c26fa3b8ad633c361b4dd5931c49cfd9a64d4d4b
mt141081 <mt141081@fhstp.ac.at>
2016-01-15 14:57:15 +0100

    First Upload
    This is my first upload.

---
+++ js/jquery.js
@@ -0,0 +1,33 @@
+/*$(document).ready(function() {
+
+    var state = true;
+    
+    $( ".project-frame" ).hover(function() {
+      if ( state ) {
+        $( this).animate({
+          opacity: '0.95',
+          color: "#fff",
+        }, 400 );
+
+      } else {
+        $( this ).animate({
+          opacity: '1',
+          color: "#000",
+        }, 400 );
+      }
+      
+      state = !state;
+    });
+  });*/
+
+
+$(document).ready(function() {
+
+    $('.project-frame').hover(function(){
+      $('.project-text',this).slideToggle('slow');
+    }, function(){
+      $('.project-text',this).slideToggle('slow');
+      });
+});
+
+    
